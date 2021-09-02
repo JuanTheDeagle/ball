41158abdcad6eede8b11d7325fde9d7da0cc589d
JuanTheDeagle <78816028+JuanTheDeagle@users.noreply.github.com>
2021-09-02 18:52:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var angle = 0
+
+let engine;
+let world;
+var ball;
+var ground;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+  var ground_options ={
+     isStatic: true
+   };
+  var wedge_options={
+    isStatic: true
+  }
+  wedge = Bodies.rectangle(100,200,100,20,wedge_options);
+  World.add(world,wedge);
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(50);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+  Body.rotate(wedge,angle)
+  push();
+  translate(wedge.position.x,wedge.position.y);
+  rotate(angle);
+  rect(0,0,100,20);
+  pop();
+  angle += 0.4
+}
+
